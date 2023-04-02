477abddb1babc53f5f8105c6d674ce8bf4481fc0
Julian Rojas <julianrojasgeo@gmail.com>
2023-04-02 18:52:47 -0500

    adapter for order added with get all and register implementations

---
+++ infrastructure/driven-adapters/mongo-repository/src/main/java/com/cafeapp/mongo/MongoRepositoryAdapterOrder.java
@@ -0,0 +1,56 @@
+package com.cafeapp.mongo;
+
+import com.cafeapp.model.order.Order;
+import com.cafeapp.model.order.gateways.OrderRepositoryGateway;
+import com.cafeapp.mongo.data.OrderData;
+import com.cafeapp.mongo.data.UserData;
+import lombok.RequiredArgsConstructor;
+import org.reactivecommons.utils.ObjectMapper;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Repository
+@RequiredArgsConstructor
+public class MongoRepositoryAdapterOrder implements OrderRepositoryGateway {
+
+    private final MongoDBRepositoryOrder orderRepository;
+    private final ObjectMapper mapper;
+
+    @Override
+    public Flux<Order> getAllOrders() {
+        return this.orderRepository
+                .findAll()
+                .switchIfEmpty(Mono.error(new Throwable("No orders available")))
+                .map(order -> mapper.map(order, Order.class))
+                .onErrorResume(Mono::error);
+    }
+
+    @Override
+    public Mono<Order> getOrderById(String id) {
+        return null;
+    }
+
+    @Override
+    public Mono<Order> getOrderByName(String id) {
+        return null;
+    }
+
+    @Override
+    public Mono<Order> registerOrder(Order order) {
+        return this.orderRepository
+                .save(mapper.map(order, OrderData.class))
+                .map(order1 -> mapper.map(order1, Order.class))
+                .onErrorResume(Mono::error);
+    }
+
+    @Override
+    public Mono<Order> updateOrder(Order order) {
+        return null;
+    }
+
+    @Override
+    public Mono<String> unregisterOrder(String id) {
+        return null;
+    }
+}
