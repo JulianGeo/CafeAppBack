abfed933ea51ec627efc1435a99c5788829c2690
Julian Rojas <julianrojasgeo@gmail.com>
2023-04-02 19:48:58 -0500

    register order usecase implemented

--- domain/usecase/src/main/java/com/cafeapp/usecase/orders/registerorder/RegisterOrderUseCase.java
+++ domain/usecase/src/main/java/com/cafeapp/usecase/orders/registerorder/RegisterOrderUseCase.java
@@ -1,6 +1,19 @@
 package com.cafeapp.usecase.orders.registerorder;
 
+import com.cafeapp.model.order.Order;
+import com.cafeapp.model.order.gateways.OrderRepositoryGateway;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
 @RequiredArgsConstructor
-public class RegisterOrderUseCase {
+public class RegisterOrderUseCase implements Function<Order, Mono<Order>> {
+
+    private final OrderRepositoryGateway repositoryGateway;
+
+    @Override
+    public Mono<Order> apply(Order order) {
+        return repositoryGateway.registerOrder(order);
+    }
 }
