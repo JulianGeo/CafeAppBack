cd0528ad39cd55f6bc11ffec2b7c76ac1e9bfc79
Julian Rojas <julianrojasgeo@gmail.com>
2023-04-02 20:14:42 -0500

    correction in this usecase

--- domain/usecase/src/main/java/com/cafeapp/usecase/users/getuserbyemail/GetUserByEmailUseCase.java
+++ domain/usecase/src/main/java/com/cafeapp/usecase/users/getuserbyemail/GetUserByEmailUseCase.java
@@ -1,6 +1,20 @@
 package com.cafeapp.usecase.users.getuserbyemail;
 
+import com.cafeapp.model.user.User;
+import com.cafeapp.model.user.gateways.UserRepositoryGateway;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
 @RequiredArgsConstructor
-public class GetUserByEmailUseCase {
+public class GetUserByEmailUseCase implements Function<String, Mono<User>> {
+
+    //TODO: refactor for get by email
+    private final UserRepositoryGateway repositoryGateway;
+
+    @Override
+    public Mono<User> apply(String email) {
+        return repositoryGateway.getUserById(email);
+    }
 }
